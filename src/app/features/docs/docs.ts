ddbb1ee96772f2c4a3e55a238b3c36a755d9356d
Josep <josep.enric.ec@gmail.com>
2026-02-25 12:39:24 +0100

    Working Docs

--- src/app/features/docs/docs.ts
+++ src/app/features/docs/docs.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterLink, RouterLinkActive } from '@angular/router';
+import { MarkdownComponent } from 'ngx-markdown';
 
 @Component({
   selector: 'app-docs',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, MarkdownComponent],
   templateUrl: './docs.html',
   styleUrl: './docs.scss',
 })
-export class Docs {}
+export class Docs implements OnInit {
+  markdownSrc: string | null = null;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      const section = params.get('section');
+      this.markdownSrc = section ? `assets/docs/${section}.md` : null;
+    });
+  }
+}
