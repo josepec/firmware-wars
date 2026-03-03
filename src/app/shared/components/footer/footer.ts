61f1b947c50ddfb78c45c90cebce8fc55ebfcdde
Josep <josep.enric.ec@gmail.com>
2026-03-03 18:54:21 +0100

    API version

--- src/app/shared/components/footer/footer.ts
+++ src/app/shared/components/footer/footer.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, signal, OnInit } from '@angular/core';
+
+const API_URL = 'https://firmware-wars-api.josepec.eu';
 
 @Component({
   selector: 'app-footer',
@@ -6,6 +8,16 @@ import { Component } from '@angular/core';
   templateUrl: './footer.html',
   styleUrl: './footer.scss',
 })
-export class Footer {
+export class Footer implements OnInit {
   readonly year = new Date().getFullYear();
+  readonly version = signal('');
+
+  ngOnInit(): void {
+    fetch(`${API_URL}/version`)
+      .then(r => r.json())
+      .then((data: { version: string | null }) => {
+        if (data.version) this.version.set(data.version);
+      })
+      .catch(() => {});
+  }
 }
